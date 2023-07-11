d2b59a72f360da7d42148119af42185a8bd8ea8f
CCliftS <90357585+CCliftS@users.noreply.github.com>
2023-07-10 23:49:40 -0400

    Add files via upload

--- login/components/Profile.tsx
+++ login/components/Profile.tsx
@@ -6,40 +6,31 @@ const Profile: React.FC = () => {
 
     const token = localStorage.getItem('token');
 
-    if(!token){
-        return <h1>Not Logged</h1>
-    }
     return(
-        <div>
-            <h1>
-                Profile
-            </h1>
+        <div className="flex h-screen w-full items-center bg-zinc-300 bg-cover text-xl">
+          <div className="BarraIZQ rounded-xl bg-[#384E55] ml-2 h-screen w-64 shadow-lg backdrop-blur-md max-sm:px-8 text-center">
+            <img src="https://static.wixstatic.com/media/8db992_3c6f3144b95c4690a72c5ea5c6bd9176~mv2.png/v1/crop/x_24,y_0,w_251,h_172/fill/w_351,h_240,al_c,lg_1,q_85,enc_auto/8db992_3c6f3144b95c4690a72c5ea5c6bd9176~mv2.png" width="150" alt="" srcSet="" className="ml-10"/>
+            <hr className="rounded-xl h-0.5 w-60 ml-2 bg--200 border-0 bg-[#DF4C17]"/>
+            <div className="flex flex-col">
+              <div className="flex flex-col mb-16">
+                <button className="mt-7 bg-[#384E55] rounded-3xl px-10 py-2 text-white backdrop-blur-md transition-colors duration-300 hover:bg-[#47636b] backdrop-blur-md">Reportes</button>
+                <button className="mt-7 bg-[#384E55] rounded-3xl px-10 py-2 text-white backdrop-blur-md transition-colors duration-300 hover:bg-[#47636b] backdrop-blur-md">Aceptar Visita</button>
+                <button className="mt-7 bg-[#384E55] rounded-3xl px-10 py-2 text-white backdrop-blur-md transition-colors duration-300 hover:bg-[#47636b] backdrop-blur-md">Factor de Carga</button>
+              </div>
+              <button className="mt-64 bg-[#384E55] rounded-3xl px-10 py-2 text-white backdrop-blur-md transition-colors duration-300 hover:bg-[#47636b] backdrop-blur-md">Cerrar Sesión</button>
+            </div>
+          </div>
+          <div className="Botones flex flex-row ml-72 mt-5 absolute inset-x-0 top-0">
+            <div className="ml-32">
+              <button className="mr-5 rounded-3xl bg-[#384E55] px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-sky-800 backdrop-blur-md">Diario</button>
+              <button className="mr-5 rounded-3xl bg-[#384E55] px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-sky-800 backdrop-blur-md">Semanal</button>
+              <button className="mr-5 rounded-3xl bg-[#384E55] px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-sky-800 backdrop-blur-md">Semanal Cerrado</button>
+              <button className="mr-5 rounded-3xl bg-[#384E55] px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-sky-800 backdrop-blur-md">Mensual</button>
+              <button className="mr-5 rounded-3xl bg-[#384E55] px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-sky-800 backdrop-blur-md">Anual</button>
+            </div>
+          </div>
         </div>
     )
-    /*
-    const  [user, setUser] = useState<UserProfile>({} as UserProfile)
-    useEffect(() => {
-        const getProfile = async () =>{
-            const ENDPOINT = 'http://localhost:3000/users/profile'
-            const config = {
-            headers: {
-                Autorization: 'Bearer ${token}'
-            }
-        }
-
-        const response = await axios.get(ENDPOINT, config)
-        const dataResponse: UserProfile = response.data
-        setUser(dataResponse)
-        }
-        const token = localStorage.getItem('token')
-        if(!token){
-            window.location.href = '/auth/login'
-        }
-    } ,[])
-    
-    return <>
-        Profile
-    </>*/
 }
 
 export default Profile
